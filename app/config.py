c4d6c22598376b2c3d341d64bc2f86da2c036171
Stephen Kaplan <stevejkaplan@gmail.com>
2020-08-20 14:52:19 -0600

    updated configuration file

--- app/config.py
+++ app/config.py
@@ -1,2 +1,9 @@
-ENVIRONMENT = 'production'     # for production, 'production'
-DEBUG = False                    # for production, False
+"""
+As of 8-20-2020 this does not exist in dev branch.
+"""
+
+
+class ProductionConfig:
+    DEVELOPMENT = False
+    DEBUG = False
+    ENVIRONMENT = 'production'     # for production, 'production'
