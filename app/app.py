e6a80e9e7978e73b0a95aa7a04a162f9cb59f7b6
Stephen Kaplan <stevejkaplan@gmail.com>
2020-08-21 11:22:54 -0600

    fixing imports for production

--- app/app.py
+++ app/app.py
@@ -4,10 +4,9 @@ import dill as pickle
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
-from db import load_mongo_collection_as_dataframe, connect_to_mongo
-from creds import USERNAME, PWD
-
-import nlp_pipeline
+from app.db import load_mongo_collection_as_dataframe, connect_to_mongo
+from app.creds import USERNAME, PWD
+from app import nlp_pipeline
 import sys
 sys.modules['nlp_pipeline'] = nlp_pipeline
 
